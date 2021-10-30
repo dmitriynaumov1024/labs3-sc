740ab70bf6b3a46be6d2c2911c509f88eaf5a465
dmitriynaumov1024 <naumov1024@gmail.com>
2021-10-30 17:08:38 +0300

    Lab6: Update Program
    Update Program to use DataConveyor instead of a bunch of separate objects

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -8,21 +8,21 @@ class Program
     /// </summary>
     static void Main(string[] args)
     {
-        IDataSource<IEnumerable<IEnumerable<int>>> 
-            dataSource = new IntArrayFromFileDataSource("input.txt");
-        IDataHandler<IEnumerable<IEnumerable<int>>, long>
-            dataHandler = new IntArrayProductCalculator();
-        IDataHandler<IEnumerable<IEnumerable<int>>, Tuple<int, int>>
-            dataSizeHandler = new IntArraySizeCalculator();
-
-        IDataSink<object> 
-            dataSink = new FileDataSink("output.txt");
-
-        Tuple<int, int> size = dataSizeHandler.Handle(dataSource.Data);
-        long result = dataHandler.Handle(dataSource.Data);
-
-        dataSink.Put(size.Item1, size.Item2);
-        dataSink.Put(result);
-        dataSink.Flush();
+        var conveyor1 = new DataConveyor<IEnumerable<IEnumerable<int>>, object> {
+            Source = new IntArrayFromFileDataSource("input.txt"),
+            Handlers = new IDataHandler<IEnumerable<IEnumerable<int>>, object>[] {
+                new NestedDataHandler<IEnumerable<IEnumerable<int>>, Tuple<int, int>, object> (
+                    new IntArraySizeCalculator(), 
+                    (sizeTuple)=> String.Format("{0} {1}", sizeTuple.Item1, sizeTuple.Item2)
+                ),
+                new NestedDataHandler<IEnumerable<IEnumerable<int>>, long, object> (
+                    new IntArrayProductCalculator(),
+                    (product)=> product
+                )
+            }, 
+            Sink = new FileDataSink("output1.log")
+        };
+        
+        conveyor1.Run();
     }
 }
