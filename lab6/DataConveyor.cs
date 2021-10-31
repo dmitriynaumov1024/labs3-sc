4b4b26a20ea7b879f16ccc6bda3fd8925850d9d5
dmitriynaumov1024 <naumov1024@gmail.com>
2021-10-31 16:04:06 +0200

    Lab6: DataConveyor: virtuals and documentation

--- lab6/DataConveyor.cs
+++ lab6/DataConveyor.cs
@@ -1,9 +1,18 @@
 using System;
 
+/// <summary>
+/// Data conveyor provides a single entry point to do all work with data:
+/// - Take data from the source,
+/// - Pass it through several handlers,
+/// - Put results into the sink.
+/// All parts of the conveyor can be replaced and conveyor can be reused 
+/// many times. This type of conveyor doesn't cache any data. To avoid 
+/// unnecessary repetitions, use CachedDataConveyor instead.
+/// </summary>
 class DataConveyor<TInput, TOutput>
 {
     public IDataSource<TInput> Source;
-    public IDataHandler<TInput, TOutput>[] Handlers;
+    public virtual IDataHandler<TInput, TOutput>[] Handlers { get; set; }
     public IDataSink<TOutput> Sink;
     
     public DataConveyor (){ }
@@ -18,7 +27,7 @@ class DataConveyor<TInput, TOutput>
         this.Sink = sink;
     }
 
-    public void Run()
+    public virtual void Run()
     {
         foreach (var handler in this.Handlers){
             TOutput outputPortion = handler.Handle (Source.Data);
